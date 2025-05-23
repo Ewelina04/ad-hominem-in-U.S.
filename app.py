8a3511d24ced8bd258624a4e789b3cfabecee012
Ewelina Gajewska <50328193+Ewelina04@users.noreply.github.com>
2025-05-23 14:33:30 +0200

    Update app.py placeholder

--- app.py
+++ app.py
@@ -92,9 +92,8 @@ if "messages" not in st.session_state.keys():
 add_spacelines(2)
 
 with st.chat_message( "human" ):
-    text = st.chat_input()
+    text = st.chat_input("Arabia? And with all of the money they have, we're defending them, and they're not paying? All you have to do is speak to them. Wait. You have so many different things you have to be able to do, and I don't believe that Hillary has the stamina.")
     st.write(text)
-
 
 
 # Start the conversation
