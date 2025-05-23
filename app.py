0e9c64e2e632dabe01cfd401d8aab76f5e7f622f
Ewelina Gajewska <50328193+Ewelina04@users.noreply.github.com>
2025-05-23 14:34:58 +0200

    Update app.py 96

--- app.py
+++ app.py
@@ -93,6 +93,8 @@ add_spacelines(2)
 
 with st.chat_message( "human" ):
     text = st.chat_input("Arabia? And with all of the money they have, we're defending them, and they're not paying? All you have to do is speak to them. Wait. You have so many different things you have to be able to do, and I don't believe that Hillary has the stamina.")
+    while text is None:
+        st.stop()
     st.write(text)
 
 
